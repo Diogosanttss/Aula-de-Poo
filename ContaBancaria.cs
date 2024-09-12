0c255f5d71671cb1a1e8563a216835b80ec965f3
diogo Conceição dos Santos <diogo.santos@escolar.ifrn.edu.br>
2024-09-12 16:28:18 -0300

    Poo
    Aula de Poo

---
+++ ContaBancaria.cs
@@ -0,0 +1,13 @@
+public class ContaBancaria{
+
+    public string titular;
+    public int saldo;
+
+    public string agencia;
+
+    public ContaBancaria(string titular, int saldo, string agencia){
+        titular = titular;
+        saldo = saldo;
+        agencia = agencia;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Aula_de_Poo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ContaBancaria conta = new ContaBancaria("Luiz",10000,"inter");
+    }
+}
